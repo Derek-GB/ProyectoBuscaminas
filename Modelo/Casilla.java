a4cde61acc404d0a5b0395584928fc6731443dc3
DylanS.S <dsegurasevilla@gmail.com>
2024-10-26 18:29:02 -0600

    Clase Casilla y Clase Estado
    Se creo una clase Casilla y una Clase Enum de estado para la clase casilla

---
+++ Modelo/Casilla.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author DYLAN
+ */
+public class Casilla {
+    private boolean esMina;
+    private Estado estado;
+    private int numero;
+
+    public boolean isEsMina() {
+        return esMina;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Casilla(boolean esMina, Estado estado, int numero) {
+        this.esMina = esMina;
+        this.estado = estado;
+        this.numero = numero;
+    }
+    
+    public Casilla() {
+        this.esMina = false;         
+        this.estado = Estado.CERRADA; 
+        this.numero = 0;             
+    }
+    
+    public void establecerMina() {
+        this.esMina = true;
+    }
+    
+    public void establecerEstado(Estado nuevoEstado) {
+        this.estado = nuevoEstado;
+    }
+    
+    public void establecerNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    public void destapar() {
+        if (estado == Estado.CERRADA) {
+            this.estado = Estado.DESTAPADA;
+        }
+    }
+    
+    public void marcar() {
+        if (estado == Estado.CERRADA) {
+            this.estado = Estado.MARCADA;
+        } else if (estado == Estado.MARCADA) {
+            this.estado = Estado.CERRADA;
+        }
+    }
+    
+}
