d0bcacb6e4338bc6fa2837879e5d19af3154209b
Tony <anconejoro@est.utn.ac.cr>
2024-11-03 12:59:29 -0600

    se hizo reloj con jilos y el metodo run
    la clase reloj falta de hacerlo mvc

--- Modelo/RelojModel.java
+++ Modelo/RelojModel.java
@@ -10,13 +10,30 @@ import java.time.LocalTime;
  *
  * @author Tony
  */
-public class RelojModel {
+public class RelojModel extends Thread {
+
+    public void run() {
+        for (int h = 0; h <= 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                for (int s = 0; s < 60; s++) {
+                    String strH = h < 10 ? "0" + h : h + "";
+                    String strM = m < 10 ? "0" + m : m + "";
+                    String strS = s < 10 ? "0" + s : s + "";
+                    JFReloj.etiReloj.SetTex(strH+":"+strM+":"+strS);
+                    try{
+                        Thread.sleep(1000);
+                    }catch(Exception e){
+                        
+                    }
+                }
+            }
+        }
+    }
 
     private static RelojModel reloj;
     private LocalTime tiempoActual;
     private boolean corriendo;
 
-    
     public void iniciar() {
         if (!corriendo) {
             this.tiempoActual = LocalTime.now();
